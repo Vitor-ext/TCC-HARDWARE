05bcb0031a6b1b3a43453a61b428e9fd5cab43ab
Vitor de Jesus <santanav92@gmail.com>
2022-06-18 19:54:16 -0300

    Avanços Dev Rovi

--- rovi.py
+++ rovi.py
@@ -5,9 +5,22 @@
 import json
 import time
 import cv2
+import socket
 
-#try:
-while (1):
+host = "127.0.0.1"  #Server address IPV4 da maquina na rede
+port = 3001 #Port of Server
+
+logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_ROVI_FIRMWARE/Logo.png', cv2.IMREAD_COLOR)
+logo = cv2.resize(logo, (int(1280), int(780)))
+cv2.imshow('ROVI-BR', logo)
+cv2.waitKey()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port)) #bind server
+s.listen(2)
+print('Aguardando conexão')
+
+while true:
     with open("Teste.json", encoding='utf-8') as teste:
         dados = json.load(teste)
     for i in dados:
@@ -26,6 +39,4 @@ while (1):
         cv2.imshow('ROVI-BR', carga)
         cv2.waitKey()
 
-#except:
- #   print("Deu Ruim")
 # largura, altura, canais = imagem.shape   #Caso queira utilizar o tamanho da imagem
