3c7b8dde4e2e6981d96ba092d35f9c6ce73ff92c
Vitor de Jesus <santanav92@gmail.com>
2022-06-20 22:59:16 -0300

    Avanços Dev Rovi

--- rovi.py
+++ rovi.py
@@ -8,35 +8,45 @@ import cv2
 import socket
 
 host = "127.0.0.1"  #Server address IPV4 da maquina na rede
-port = 3001 #Port of Server
-
-logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_ROVI_FIRMWARE/Logo.png', cv2.IMREAD_COLOR)
-logo = cv2.resize(logo, (int(1280), int(780)))
-cv2.imshow('ROVI-BR', logo)
-cv2.waitKey()
+port = 1235 #Port of Server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port)) #bind server
-s.listen(2)
+s.listen(10)
 print('Aguardando conexão')
 
-while true:
-    with open("Teste.json", encoding='utf-8') as teste:
-        dados = json.load(teste)
-    for i in dados:
-        valor = (i['valor'])
-    print(valor)
+#logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_FIR_FRONT/Logo.png', cv2.IMREAD_COLOR)
+#logo = cv2.resize(logo, (int(1280), int(780)))
+#cv2.imshow('ROVI-BR', logo)
+#cv2.waitKey()
 
-    if (valor==0):
-        logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_ROVI_FIRMWARE/Logo.png', cv2.IMREAD_COLOR)
-        logo = cv2.resize(logo,(int(1280),int(780)))
+
+
+while True:
+
+    #with open("Teste.json", encoding='utf-8') as teste:
+    #    dados = json.load(teste)
+    #   for i in dados:
+    #        valor = (i['valor'])
+    #    print(valor)
+    conn, addr = s.accept()
+    dados = (str.encode('dados recebido com sucesso'))
+    conn.sendall(dados)
+    valor = conn.recv(1)
+    print(valor.decode())
+    teste  = int (valor.decode())
+
+    if (teste == 0):
+        logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_FIR_FRONT/Logo.png', cv2.IMREAD_COLOR)
+        logo = cv2.resize(logo, (int(1280), int(780)))
         cv2.imshow('ROVI-BR', logo)
         cv2.waitKey()
 
     else:
-        carga = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_ROVI_FIRMWARE/Carga.png', cv2.IMREAD_COLOR)
-        carga = cv2.resize(carga,(int(1280),int(780)))
+        carga = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_FIR_FRONT/Carga.png', cv2.IMREAD_COLOR)
+        carga = cv2.resize(carga, (int(1280), int(780)))
         cv2.imshow('ROVI-BR', carga)
         cv2.waitKey()
 
-# largura, altura, canais = imagem.shape   #Caso queira utilizar o tamanho da imagem
+
+print('Conexão Fechada')
