48550a42cbb7f21d100b29199e5db3e42b265760
Vitor de Jesus <santanav92@gmail.com>
2022-07-12 09:04:20 -0300

    Dev Rovi

--- rovi.py
+++ rovi.py
@@ -12,19 +12,19 @@ port = 1235 #Port of Server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port)) #bind server
-s.listen(10)
+s.listen(100)
 print('Aguardando conexão')
 
-#logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_FIR_FRONT/Logo.png', cv2.IMREAD_COLOR)
-#logo = cv2.resize(logo, (int(1280), int(780)))
-#cv2.imshow('ROVI-BR', logo)
-#cv2.waitKey()
+logo = cv2.imread('C:/Users/vitor.jesus/Desktop/Dev/TCC_FIR_FRONT/Logo.png', cv2.IMREAD_COLOR)
+logo = cv2.resize(logo, (int(1280), int(780)))
+cv2.imshow('ROVI-BR', logo)
+cv2.waitKey()
 
 
 
 while True:
 
-    #with open("Teste.json", encoding='utf-8') as teste:
+    #with open("Teste.json", encoding='utf-8') as teste:     # Teste com Json
     #    dados = json.load(teste)
     #   for i in dados:
     #        valor = (i['valor'])
